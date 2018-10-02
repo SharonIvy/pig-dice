d18abb711384d9fd41c16fe6802a116e504ac9a3
SharonIvy <kendishivy72@gmail.com>
2018-10-02 04:46:09 +0300

    Add User Interface for interaction

--- js/scripts.js
+++ js/scripts.js
@@ -43,3 +43,67 @@ Player.prototype.restartGame = function () {
   this.playerName = "";
 }
 
+//User Interface:
+$(document).ready(function () {
+
+  $("button#startButton").click(function (event) {
+    playerOne = new Player(true);
+    playerTwo = new Player(false);
+    $(".console").show();
+    $(".start").hide();
+
+    var playerOneName = $(".playerOneName").val();
+    $("#playerOneName").text(playerOneName);
+
+    var playerTwoName = $(".playerTwoName").val();
+    $("#playerTwoName").text(playerTwoName);
+
+    playerOne.playerName = playerOneName;
+    playerTwo.playerName = playerTwoName;
+
+  });
+  $("button#restartGame").click(function (event) {
+    $(".console").hide();
+    $(".start").show();
+    clearValues();
+    playerOne.restartGame();
+    playerTwo.restartGame();
+    $("#playerOneRoundTotal").empty();
+    $("#playerOneTotalScore").empty();
+    $("#playerOneDiceRoll").empty();
+    $("#playerTwoRoundTotal").empty();
+    $("#playerTwoTotalScore").empty();
+    $("#playerTwoDiceRoll").empty();
+  });
+
+  $("button#playerOneRoll").click(function (event) {
+    playerOne.roll = dice();
+    $("#playerOneDiceRoll").text(playerOne.roll);
+    playerOne.rollone();
+    $("#playerOneRoundTotal").text(playerOne.score);
+  });
+
+  $("button#playerTwoRoll").click(function (event) {
+    playerTwo.roll = dice();
+    $("#playerTwoDiceRoll").text(playerTwo.roll);
+    playerTwo.rollone();
+    $("#playerTwoRoundTotal").text(playerTwo.score);
+  });
+
+  $("button#playerOneHold").click(function (event) {
+    playerOne.hold();
+    $("#playerOneTotalScore").text(playerOne.totalScore);
+    $("#playerOneRoundTotal").empty();
+    $("#playerOneDiceRoll").empty();
+    playerOne.winner();
+  });
+
+  $("button#playerTwoHold").click(function (event) {
+    playerTwo.hold();
+    $("#playerTwoTotalScore").text(playerTwo.totalScore);
+    $("#playerTwoRoundTotal").empty();
+    $("#playerTwoDiceRoll").empty();
+    playerTwo.winner();
+  });
+
+});
